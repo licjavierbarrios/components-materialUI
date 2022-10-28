c0bcb6c988c10783967e0b7641bcf5ace82cd1bc
licjavierbarrios <licjavierbarrios@hotmail.com>
2022-10-28 09:11:11 -0300

    feat: MuiNavbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import MuiImageList from "./components/MuiImageList";
+// import MuiNavbar from "./components/MuiNavbar";
+// import MuiImageList from "./components/MuiImageList";
 // import MuiAccordion from './components/MuiAccordion'
 // import MuiCard from './components/MuiCard'
 // import MuiLayout from './components/MuiLayout'
@@ -29,7 +30,8 @@ const App = () => {
 			{/* <MuiLayout /> */}
 			{/* <MuiCard /> */}
 			{/* <MuiAccordion /> */}
-			<MuiImageList />
+			{/* <MuiImageList /> */}
+			{/* <MuiNavbar /> */}
 		</div>
 	);
 };
