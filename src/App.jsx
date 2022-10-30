ea39cca1e3297a3d82f4ae5b48f238695d2904be
licjavierbarrios <licjavierbarrios@hotmail.com>
2022-10-30 14:47:12 -0300

    Componentes updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import React from "react";
+import { createTheme, colors, ThemeProvider } from "@mui/material";
 import "./App.css";
-import MuiDrawer from "./components/MuiDrawer";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import MuiResponsiveness from "./components/MuiResponsiveness";
+
+// import MuiPicker from "./components/MuiPicker";
+// import MuiTabs from "./components/MuiTabs";
+// import MuiLoadingButton from "./components/MuiLoadingButton";
+// import MuiSkeleton from "./components/MuiSkeleton";
+// import MuiTable from "./components/MuiTable";
+// import MuiDialog from "./components/MuiDialog";
+// import MuiSnackbars from "./components/MuiSnackbars";
+// import MuiChip from "./components/MuiChip";
+// import MuiBadge from "./components/MuiBadge";
+// import MuiList from "./components/MuiList";
+// import MuiAvatar from "./components/MuiAvatar";
+// import MuiBottomNavigation from "./components/MuiBottomNavigation";
+// import MuiDrawer from "./components/MuiDrawer";
 // import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
 // import MuiLink from "./components/MuiLink";
-import MuiNavbar from "./components/MuiNavbar";
-import MuiSpeedDial from "./components/MuiSpeedDial";
+// import MuiNavbar from "./components/MuiNavbar";
+// import MuiSpeedDial from "./components/MuiSpeedDial";
 // import MuiImageList from "./components/MuiImageList";
-// import MuiAccordion from './components/MuiAccordion'
-// import MuiCard from './components/MuiCard'
-// import MuiLayout from './components/MuiLayout'
-// import MuiAutocomplete from './components/MuiAutocomplete'
-// import MuiRating from './components/MuiRating'
-// import MuiSwitch from './components/MuiSwitch'
-// import MuiCheckBox from './components/MuiCheckBox'
-// import MuiRadioButton from './components/MuiRadioButton'
-// import MuiSelect from './components/MuiSelect'
-// import MuiTextField from './components/MuiTextField'
-// import MuiButton from './components/MuiButton'
-// import MuiTypography from './components/MuiTypography'
+// import MuiAccordion from "./components/MuiAccordion";
+// import MuiCard from "./components/MuiCard";
+// import MuiLayout from "./components/MuiLayout";
+// import MuiAutocomplete from "./components/MuiAutocomplete";
+// import MuiRating from "./components/MuiRating";
+// import MuiSwitch from "./components/MuiSwitch";
+// import MuiCheckBox from "./components/MuiCheckBox";
+// import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiSelect from "./components/MuiSelect";
+// import MuiTextField from "./components/MuiTextField";
+// import MuiButton from "./components/MuiButton";
+// import MuiTypography from "./components/MuiTypography";
+
+const theme= createTheme({
+	status: {
+		danger: '#e53e3e',
+	},
+	palette: {
+		secondary: {
+			main: colors.orange[500],
+		},
+	},	
+})
 
 const App = () => {
 	return (
@@ -35,11 +63,28 @@ const App = () => {
 			{/* <MuiCard /> */}
 			{/* <MuiAccordion /> */}
 			{/* <MuiImageList /> */}
-			<MuiNavbar />
+			{/* <MuiNavbar /> */}
 			{/* <MuiLink /> */}
 			{/* <MuiBreadcrumbs /> */}
-			<MuiDrawer />
-			<MuiSpeedDial />
+			{/* <MuiDrawer /> */}
+			{/* <MuiSpeedDial /> */}
+			{/* <MuiBottomNavigation /> */}
+			{/* <MuiAvatar /> */}
+			{/* <MuiBadge /> */}
+			{/* <MuiList /> */}
+			{/* <MuiChip /> */}
+			{/* <MuiSnackbars /> */}
+			{/* <MuiDialog /> */}
+			{/* <MuiTable /> */}
+			{/* <MuiSkeleton /> */}
+			{/* <MuiLoadingButton /> */}
+			{/* <LocalizationProvider dateAdapter={AdapterDateFns}> */}
+			{/* <MuiPicker /> */}
+			{/* <MuiTabs /> */}
+			{/* </LocalizationProvider> */}
+			<ThemeProvider theme={theme}>
+				<MuiResponsiveness />
+			</ThemeProvider>
 		</div>
 	);
 };
